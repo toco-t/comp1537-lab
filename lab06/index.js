f819b27076dec50d19a423ac00a20d65f22d904d
Toco Tachibana <ttachibana@my.bcit.ca>
2022-02-25 00:01:11 -0800

    Update lab06 - calculator
    Update lab06 - calculator

--- lab06/index.js
+++ lab06/index.js
@@ -5,26 +5,36 @@ $("button").click(function() {
   if ($(this).data("val") == "+") {
     var result = parseFloat(first_operand) + parseFloat(second_operand)
     $(".result").html("Result: " + first_operand + " + " + second_operand + " = " + result)
-    $(".history").append("<div><span class='red'>" + first_operand + " + " + second_operand + " = " + result + "</span></div>")
+    $(".history").append("<div class='red'>" + first_operand + " + " + second_operand + " = " + result + "</div>")
   }
 
   else if ($(this).data("val") == "-") {
     var result = parseFloat(first_operand) - parseFloat(second_operand)
     $(".result").html("Result: " + first_operand + " - " + second_operand + " = " + result)
-    $(".history").append("<div><span class='blue'>" + first_operand + " - " + second_operand + " = " + result + "</span></div>")
+    $(".history").append("<div class='blue'>" + first_operand + " - " + second_operand + " = " + result + "</div>")
   }
 
   else if ($(this).data("val") == "*") {
     var result = parseFloat(first_operand) * parseFloat(second_operand)
     $(".result").html("Result: " + first_operand + " * " + second_operand + " = " + result)
-    $(".history").append("<div><span class='green'>" + first_operand + " * " + second_operand + " = " + result + "</span></div>")
+    $(".history").append("<div class='green'>" + first_operand + " * " + second_operand + " = " + result + "</div>")
+  }
+
+  else if ($(this).data("val") == "/") {
+    var result = parseFloat(first_operand) / parseFloat(second_operand)
+    $(".result").html("Result: " + first_operand + " / " + second_operand + " = " + result)
+    $(".history").append("<div class='yellow'>" + first_operand + " / " + second_operand + " = " + result + "</div>")
+  }
+
+  else if ($(this).data("val") == "^") {
+    var result = parseFloat(first_operand) ^ parseFloat(second_operand)
+    $(".result").html("Result: " + first_operand + " ^ " + second_operand + " = " + result)
+    $(".history").append("<div class='purple'>" + first_operand + " ^ " + second_operand + " = " + result + "</div>")
   }
 
   else {
-    var result = parseFloat(first_operand) / parseFloat(second_operand)
-    $(".result").html("Result: " + first_operand + " / " + second_operand + " = " + result)
-    $(".history").append("<div><span class='yellow'>" + first_operand + " / " + second_operand + " = " + result + "</span></div>")
+    var result = parseFloat(first_operand) % parseFloat(second_operand)
+    $(".result").html("Result: " + first_operand + " % " + second_operand + " = " + result)
+    $(".history").append("<div class='gold'>" + first_operand + " % " + second_operand + " = " + result + "</div>")
   }
-
-
 });
