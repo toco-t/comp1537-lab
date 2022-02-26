3720c0761888541afbd9fbaebd1f99779e015a15
Toco Tachibana <ttachibana@my.bcit.ca>
2022-02-25 16:13:55 -0800

    Update lab06
    Update lab06

--- lab06/index.js
+++ lab06/index.js
@@ -32,9 +32,19 @@ $("button").click(function() {
     $(".history").append("<div class='purple'>" + first_operand + " ^ " + second_operand + " = " + result + "</div>")
   }
 
-  else {
+  else if ($(this).data("val") == "%") {
     var result = parseFloat(first_operand) % parseFloat(second_operand)
     $(".result").html("Result: " + first_operand + " % " + second_operand + " = " + result)
     $(".history").append("<div class='gold'>" + first_operand + " % " + second_operand + " = " + result + "</div>")
+  }
+
+  else if ($(this).data("val") == "font+") {
+    var bigger = parseInt($(".history>div").css("font-size") ) * 1.2
+    $(".history>div").css("font-size", bigger)
+  }
+
+  else {
+    var smaler = parseInt($(".history>div").css("font-size") ) * 0.8
+    $(".history>div").css("font-size", smaler)
   }
 });
